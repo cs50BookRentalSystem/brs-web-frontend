b19f8dd7e1b4926d7d19514647b868462560227e
dannykhant <dannypmkhant@gmail.com>
2025-07-26 21:40:15 +0700

    Add confirmation on deleting student

--- src/components/StudentTable.jsx
+++ src/components/StudentTable.jsx
@@ -2,11 +2,15 @@ import { Box, IconButton } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { Delete as DeleteIcon } from "@mui/icons-material";
 
+import ConfirmDialog from "./ConfirmDialog";
+import { useState } from "react";
+
 export default function StudentTable({
   rows,
   paginationModel,
   setPaginationModel,
 }) {
+  const [openDialog, setOpenDialog] = useState(false);
   const columns = [
     { field: "first_name", headerName: "First Name", flex: 1 },
     { field: "last_name", headerName: "Last Name", flex: 1 },
@@ -22,6 +26,7 @@ export default function StudentTable({
         return (
           <IconButton
             onClick={() => {
+              setOpenDialog(true);
               console.log(params.row.id);
             }}
             color="error"
@@ -34,18 +39,26 @@ export default function StudentTable({
     },
   ];
   return (
-    <Box sx={{ mt: 3 }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pagination
-        paginationModel={paginationModel}
-        onPaginationModelChange={setPaginationModel}
-        pageSizeOptions={[5, 10]}
-        disableRowSelectionOnClick
-        disableColumnSelector
-        disableDensitySelector
+    <>
+      <Box sx={{ mt: 3 }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pagination
+          paginationModel={paginationModel}
+          onPaginationModelChange={setPaginationModel}
+          pageSizeOptions={[5, 10]}
+          disableRowSelectionOnClick
+          disableColumnSelector
+          disableDensitySelector
+        />
+      </Box>
+      <ConfirmDialog
+        msg="Are you sure you want to delete it?"
+        open={openDialog}
+        onClose={() => setOpenDialog(false)}
+        onConfirm={() => {}}
       />
-    </Box>
+    </>
   );
 }

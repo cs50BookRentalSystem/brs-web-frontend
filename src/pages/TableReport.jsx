fa1d236f0b7a41f3bc997da71ee2798484ce55e1
dannykhant <dannypmkhant@gmail.com>
2025-07-29 23:58:19 +0700

    Add pagination on table report

--- src/pages/TableReport.jsx
+++ src/pages/TableReport.jsx
@@ -1,31 +1,50 @@
-import { Box, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TextField,
+  Typography,
+  Paper,
+  Table,
+  TableRow,
+  TableBody,
+  TablePagination,
+  Alert,
+} from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
 
-import { DataGrid } from "@mui/x-data-grid";
 import { useState } from "react";
+import dayjs from "dayjs";
+
+const api = import.meta.env.VITE_API;
+const LIMIT = 10;
 
 export default function TableReport() {
-  const [filters, setFilters] = useState({
-    book: "",
-    student: "",
-  });
-  const columns = [
-    {
-      field: "book",
-      headerName: "Book Name",
-      flex: 1,
-      renderHeader: () => {
-        <TextField size="small" placeholder="Search by Book" />;
-      },
-    },
-    { field: "student", headerName: "Student Name", flex: 1 },
-    { field: "date", headerName: "Date Rented", flex: 1 },
-  ];
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * LIMIT;
 
-  const rows = [
-    { id: 1, book: "Harry Potter", student: "Elon Musk", date: "2025-01-01" },
-    { id: 2, book: "Harry Potter", student: "Elon Musk", date: "2025-01-01" },
-    { id: 3, book: "Harry Potter", student: "Elon Musk", date: "2025-01-01" },
-  ];
+  const { isError, isLoading, error, data, refetch } = useQuery({
+    queryKey: ["rents", page],
+    queryFn: async () => {
+      const res = await fetch(`${api}/rents?limit=${LIMIT}&offset=${offset}`, {
+        credentials: "include",
+      });
+      return res.json();
+    },
+  });
+
+  if (isError) {
+    return (
+      <Box>
+        <Alert severity="warning">{error.message}</Alert>
+      </Box>
+    );
+  }
+
+  if (isLoading) {
+    return <Box sx={{ textAlign: "center" }}>Loading...</Box>;
+  }
 
   return (
     <>
@@ -41,14 +60,67 @@ export default function TableReport() {
         </Typography>
       </Box>
       <Box sx={{ mt: 3 }}>
-        <DataGrid
-          columns={columns}
-          rows={rows}
-          pageSize={10}
-          disableRowSelectionOnClick
-          disableColumnSelector
-          disableDensitySelector
-        />
+        <Paper>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Book Name</TableCell>
+                  <TableCell>Student Name</TableCell>
+                  <TableCell>Date Rented</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <TextField
+                      variant="outlined"
+                      placeholder="Search by Book Name"
+                      size="small"
+                      fullWidth
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      variant="outlined"
+                      placeholder="Search by Student Name"
+                      size="small"
+                      fullWidth
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      type="date"
+                      variant="outlined"
+                      placeholder="Search by Date Rented"
+                      size="small"
+                      fullWidth
+                    />
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.results.map((row) => (
+                  <TableRow key={row.rent_id}>
+                    <TableCell>{row.book_title}</TableCell>
+                    <TableCell>{row.student_name}</TableCell>
+                    <TableCell>
+                      {dayjs(row.rented_date).format("YYYY-MM-DD")}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            component={"div"}
+            count={data.pagination.total}
+            page={page - 1}
+            onPageChange={(e, newPage) => {
+              setPage(newPage + 1);
+            }}
+            rowsPerPage={10}
+            rowsPerPageOptions={[]}
+          />
+        </Paper>
       </Box>
     </>
   );
